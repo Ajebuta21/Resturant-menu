e4c9aecc7c44ec6ccce1fa01b84ee04c7e364602
Ajebuta21 <elvisluckyest@gmail.com>
2023-03-24 20:31:28 +0100

    first commit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
-
 const Home = () => {
   return (
     <div className={`w-full h-screen pt-[8vh] flex flex-col`}>
-      <div className="max-w-fit h-[42vh] flex flex-col mx-auto">
+      <div className="max-w-fit h-2/5 flex flex-col mx-auto">
         <h1 className="font-lobster text-6xl mt-16 ">Smokey house</h1>
         <p className="flex justify-end w-full text-lg font-edu">
           Grill & Chill
@@ -17,19 +16,33 @@ const Home = () => {
         initial={{ opacity: 0.1 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1.5, ease: "linear" }}
-        className="w-full grid grid-cols-1 h-[50vh]"
+        className="w-full h-3/5 px-2 flex flex-col items-center justify-around md:flex-row"
       >
         <Link
           to="/food-menu"
-          className="w-full h-full bg-food bg-contain flex items-center justify-end pt-20 pr-4 text-5xl text-white font-Bebas hover:opacity-60"
+          className="w-4/5 md:w-1/4 h-[25vh] md:h-[40vh] border-l border-b rounded-md shadow-md flex flex-col hover:opacity-60 transition ease-in-out"
         >
-          Food
+          <img
+            src="./food.PNG"
+            alt=""
+            className="rounded-tr-md rounded-tl-md w-full h-3/4"
+          />
+          <span className="w-full flex justify-end font-Bebas text-xl pr-4">
+            Food menu
+          </span>
         </Link>
         <Link
           to="/drinks-menu"
-          className="w-full h-full bg-drinks bg-contain flex items-center justify-end pt-20 pr-4 text-5xl text-white font-Bebas hover:opacity-60"
+          className="w-4/5 md:w-1/4 h-[25vh] md:h-[40vh] border-l border-b rounded-md shadow-md flex flex-col hover:opacity-60 transition ease-in-out"
         >
-          Drinks
+          <img
+            src="./drinks.PNG"
+            alt=""
+            className="rounded-tr-md rounded-tl-md w-full h-3/4"
+          />
+          <span className="w-full flex justify-end font-Bebas text-xl pr-4">
+            Drinks menu
+          </span>
         </Link>
       </motion.div>
     </div>
